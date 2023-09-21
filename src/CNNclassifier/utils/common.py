6a7db619e565a3151e317291e8294c850eb58eb0
AdhithyaGN <adhithyagnair97@gmail.com>
2023-09-21 11:08:56 +0530

    test commit

--- src/CNNclassifier/utils/common.py
+++ src/CNNclassifier/utils/common.py
@@ -5,18 +5,18 @@ from src.CNNclassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
-from box import Configbox
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
 
 @ensure_annotations
-def read_yaml(path_to_yaml:Path)->Config_box:
+def read_yaml(path_to_yaml:Path)->ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file:{path_to_yaml} loaded successfully")
-            return Configbox(content)
+            return ConfigBox(content)
 
     except BoxValueError:
         raise ValueError('yaml file is empty')
@@ -34,14 +34,14 @@ def create_directories(path_to_directories:list,verbose=True):
 def save_json(path:Path,data:dict):
      with open(path,'w') as f:
          json.dump(data, f ,indent=4)
-    logger.info(f'json file saved at :{path}')
+     logger.info(f'json file saved at :{path}')
 
 @ensure_annotations
-def load_json(path:Path)->Configbox:
+def load_json(path:Path)->ConfigBox:
     with open(path) as f:
         content=json.load(f)
     logger.info(f'json fike loaded succesfully from :{path}')
-    return Configbox(content)
+    return ConfigBox(content)
 
 
 @ensure_annotations
